2a2a7eddffac761ec605a6ff4e5e3de021a62041
ajaykumar-bg <ajaykumarbg.tvm@gmail.com>
2025-08-14 22:50:30 +0530

    Renamed crud-example folder to travel-planner

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,30 +1,30 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import Home from "./components/home/Home";
-import ContextExample from "./components/context-example/ContextExample";
-import PropDrillingExample from "./components/prop-drilling/PropDrillingExample";
-import Timer from "./components/timer/Timer";
-import PriceCalculator from "./components/price-calculator/PriceCalculator";
-import Message from "./components/message/Message";
-import NestedObjectViewer from "./components/nested-object-viewer/NestedObjectViewer";
-import ReactPortalExample from "./components/react-portal/ReactPortalExample";
-import TravelPlannerExample from "./components/crud-example/TravelPlannerExample";
+import Home from './components/home/Home';
+import ContextExample from './components/context-example/ContextExample';
+import PropDrillingExample from './components/prop-drilling/PropDrillingExample';
+import Timer from './components/timer/Timer';
+import PriceCalculator from './components/price-calculator/PriceCalculator';
+import Message from './components/message/Message';
+import NestedObjectViewer from './components/nested-object-viewer/NestedObjectViewer';
+import ReactPortalExample from './components/react-portal/ReactPortalExample';
+import TravelPlannerExample from './components/travel-planner/TravelPlannerExample';
 
 function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/context-example" element={<ContextExample />} />
-      <Route path="/prop-drilling" element={<PropDrillingExample />} />
-      <Route path="/nested-object-viewer" element={<NestedObjectViewer />} />
-      <Route path="/price-calculator" element={<PriceCalculator />} />
-      <Route path="/timer-example" element={<Timer />} />
-      <Route path="/message" element={<Message />} />
-      <Route path="/react-portals" element={<ReactPortalExample />} />
-      <Route path="/travel-planner" element={<TravelPlannerExample />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/context-example' element={<ContextExample />} />
+			<Route path='/prop-drilling' element={<PropDrillingExample />} />
+			<Route path='/nested-object-viewer' element={<NestedObjectViewer />} />
+			<Route path='/price-calculator' element={<PriceCalculator />} />
+			<Route path='/timer-example' element={<Timer />} />
+			<Route path='/message' element={<Message />} />
+			<Route path='/react-portals' element={<ReactPortalExample />} />
+			<Route path='/travel-planner' element={<TravelPlannerExample />} />
+		</Routes>
+	);
 }
 
 export default AppRoutes;
